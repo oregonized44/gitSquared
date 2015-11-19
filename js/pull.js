5b1b9b10fe107c3fce9df64c40dd7724661804ab
Connor Cummings <cpccummings@gmail.com>
2015-11-19 15:22:59 -0800

    merging my lander page, as well, added pull js and pull html to add to our lesson 3 module

---
+++ js/pull.js
@@ -0,0 +1,15 @@
+var solidJob = function(){
+  document.getElementById("L3GotIt").innerHTML = "Great job. You may continue on ahead!";
+  L3GotIt.src = "assets/images/niceWork.gif";
+}
+var reread = function(){
+  document.getElementById("L3DontGotIt").innerHTML = "Please re-read this section beginning at the top. In order to understand git pull fully, you must have an understanding of how 'git fetch' and 'git merge' work!"
+  L3DontGotIt.src = "assets/images/panic.gif"
+}
+
+
+
+
+
+
+
